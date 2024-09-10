b8cde52fb891b4c979610ec1f96a2db3cbe5c2ab
Yuktee Gupta <16yuktee@gmail.com>
2024-09-10 19:55:30 +1000

    trying commit from laptop2

--- PepsTester.jl
+++ PepsTester.jl
@@ -23,33 +23,4 @@ for i=1:no_cavs
     peps[2,i] = S*V
 end
 
-peps[1,3]
-
-
-i = 1
-op = ITensors.op("I", site_1[i]) * ITensors.op("I", site_2[i])
-#now to multiply then svd then store 
-mult_term = mpo_1[i] * mpo_2[i]
-# inds3 = append!(uniqueinds(mpo_1[i] , mpo_1[i+1]), uniqueinds(mpo_2[i] , mpo_2[i+1]))
-# linkind(mpo_1[i])
-inds_u = inds(mpo_1[1])
-
-U,S,V = ITensors.svd(mult_term, inds_u[1:end], cutoff=1E-8)
-U
-
-S*V
-peps[1,1] = U
-peps[2,1] = S*V
-
-inds(mpo_1[1])
-
-
-inds_u[1:2]
-u,s,v = svd(mpo_1[1], inds_u[1:2])
-U
-
-v
-inds(mult_term)
-
-
 @visualise  mpo_1

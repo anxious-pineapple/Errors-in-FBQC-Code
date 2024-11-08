23dc3d8815de925f02e759fa2922ff4ee98ccee7
anxious-pineapple <16yuktee@gmail.com>
2024-11-08 19:44:01 +1100

    Cleaned up graph generation scripts

--- GraphGen_AvgBondDim_MPOsize.jl
+++ GraphGen_AvgBondDim_MPOsize.jl
@@ -1,3 +1,10 @@
+#
+    # Code generates the graph of avg bond dim vs no of cavities
+    # New graph is generated and saved in /Plots
+    # Inputs : max_cavs rest is default, for fixed dephasing 2%
+    # can also generate a plot of how max bond dim changes with no of cavities
+#
+
 using ITensors, Plots
 import MPOFuncs
 using CurveFit
@@ -14,8 +21,9 @@ max_bond_dim = [;]
 mpo_size = [;]
 avg_bond_dim_doubl = [;]
 mpo_size_doubl = [;]
+max_cavs = 15
 
-for no_cavs in 2:15
+for no_cavs in 2:max_cavs
 
     println("No cavs ",  no_cavs)
     mpo, sites , eigenvals = MPOFuncs.cash_karppe_evolve_test(no_cavs, dep, gamma, dt, t_final)
@@ -42,19 +50,19 @@ p=plot(avg_bond_dim, label="before bmsplt")
 plot!(avg_bond_dim_doubl./2, label="after")
 plot!(max_bond_dim, label="max bond dim")
 
-a,b = linear_fit([2:15;], avg_bond_dim)
-plot!(a .+ b*[2:15;] ;label="", line=:dash)
-a2 ,b2 = linear_fit([2:15;], avg_bond_dim_doubl./2)
-plot!(a2 .+ b2*[2:15;] ; label="", line=:dash)
+a,b = linear_fit([2:max_cavs;], avg_bond_dim)
+plot!(a .+ b*[2:max_cavs;] ;label="", line=:dash)
+a2 ,b2 = linear_fit([2:max_cavs;], avg_bond_dim_doubl./2)
+plot!(a2 .+ b2*[2:max_cavs;] ; label="", line=:dash)
 plot!(xlabel="Number of cavities", ylabel="Average bond dimension")
 Plots.pdf(p, "bond_dim_linearPlot.pdf")
 
-p2= plot(mpo_size./[2:15;])
-plot!(mpo_size_doubl./(2*[2:15;]) )
+p2= plot(mpo_size./[2:max_cavs;])
+plot!(mpo_size_doubl./(2*[2:max_cavs;]) )
 
-a,b = linear_fit([2:15;], mpo_size./[2:15;])
-plot!(a .+ b*[2:15;] ;label="", line=:dash)
-a2 ,b2 = linear_fit([2:15;], mpo_size_doubl./(2*[2:15;]))
-plot!(a2 .+ b2*[2:15;] ; label="", line=:dash)
+a,b = linear_fit([2:max_cavs;], mpo_size./[2:max_cavs;])
+plot!(a .+ b*[2:max_cavs;] ;label="", line=:dash)
+a2 ,b2 = linear_fit([2:max_cavs;], mpo_size_doubl./(2*[2:max_cavs;]))
+plot!(a2 .+ b2*[2:max_cavs;] ; label="", line=:dash)
 plot!(xlabel="Number of cavities", ylabel="MPO size ")
 Plots.pdf(p2, "mposize_linearPlot.pdf")
--- GraphGen_CavDephError.jl
+++ GraphGen_CavDephError.jl
@@ -1,13 +1,24 @@
+#
+    # Code generates the graph of relative error in distinguishability vs dephasing rate for 2 to max_cav cavities
+    # New graph is generated and saved in /Plots 
+    # Inputs : max_cavs rest is default
+    # Outputs : RelativeAnalyticalErrorPerCavs.pdf
+    # can also generate heatmaps of relative error, relevant to find no of cavities needed for
+    # say less than 5% relative error
+    # can also generate heatmap/contour graphs of actual error
+
+#
+
 using Plots
 import MPOFuncs
 
 gamma = 1
 dt = 0.01
 t_final = 10
-dep_array = [0:0.001:0.03;]
+dep_array = [0:0.01:0.50;]
 max_cav = 21
 data_array = Array{Float64}(undef, size(dep_array)[1], max_cav-1)
-
+err_array = Array{Float64}(undef, size(dep_array)[1], max_cav-1)
 
 for dep in dep_array
     gf, eival = MPOFuncs.g2(gamma, dep, t_final, dt, 2; reverse=false)
@@ -18,12 +29,27 @@ for dep in dep_array
         err  = ideal - (0.5*(1 - (sum(eival[1:no_cavs].^2)/(sum(eival[1:no_cavs])^2))))
         relerr = 100 * err/ideal
         data_array[ findall(x->x==dep,dep_array)[1] , no_cavs-1] = relerr
+        err_array[ findall(x->x==dep,dep_array)[1] , no_cavs-1] = err
     end
 end
 
+# deph_array[1,:] ignored below, as for very low dephasing, the entire wave function is absorbed 
+# in the first cavity, and the error is dominated by any photon number lost in the simulation 
+# Giving rise to very high relative error, 175%+, but this is again just cause the ideal g_34 is very small itself
 
+###
+    p = plot(dep_array[2:end], abs.((data_array))[2:end,1:end])
+    plot!(xlabel="Dephasing Rate", ylabel="Relative Error (in g34)", legend = :outertopright )
+    Plots.savefig(p, "Plots/RelativeAnalyticalErrorPerCavs.pdf")
+#
 
-p = plot(heatmap(abs.(data_array)))
-Plots.savefig(p, "Plots/DephasingErrorHeatmap.pdf")
-p2 = plot(heatmap(abs.(data_array).<5))
-Plots.savefig(p2, "Plots/DephasingErrorHeatmapLessthan5.pdf")
+p2 = plot(heatmap(dep_array[2:end], [2:max_cav;], abs.(transpose(data_array))[:,2:end] .<5))
+plot!(xlabel="Dephasing Rate", ylabel="Number of Cavities" , yticks=[2:2:max_cav;])
+# Plots.savefig(p2, "Plots/DephasingErrorHeatmapLessthan5.pdf")
+
+p3 = plot(heatmap(dep_array[1:end], [2:max_cav;], abs.(transpose(err_array))[:,1:end]))
+plot!(xlabel="Dephasing Rate", ylabel="Number of Cavities" , yticks=[2:2:max_cav;])
+# Plots.savefig(p2, "Plots/DephasingErrorHeatmapLessthan5.pdf")
+
+## alt graph for error
+# contour(dep_array[1:end], [2:max_cav;], abs.(transpose(err_array))[:,1:end], clabels=true)
